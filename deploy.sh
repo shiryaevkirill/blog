2dc78653717475cc67bc7d8ec60062999cb55026
shiryaevkirill <1032182518@pfur.ru>
2020-05-23 02:44:20 +0300

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Stop, if a command fails
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...033[0m\n"
+
+# Building the project
+hugo
+cd public
+
+git add .
+msg="rebuilding site $(date)"
+if [ -n "$*" ]
+then
+	msg="$*"
+fi
+
+git commit -am "$msg"
+git push origin master
